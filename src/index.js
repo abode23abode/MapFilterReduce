5586ebc9f3b1fe3fb5300cb02726b36b76a4dc7d
Abdulilah <abdulilahalhamwi@gmail.com>
2024-03-29 00:30:07 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+
+//Filter - Create a new array by keeping the items that return true.
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+//Find - find the first item that matches from an array.
+
+//FindIndex - find the index of the first item that matches.
+
+import emojipedias from "./emojipedia";
+function meaning (emojipedia){
+    return (emojipedia.meaning.substring(0,100))
+}
+const sui= emojipedias.map(meaning);
+console.log(sui)
